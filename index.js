40dfa2675945d9360cbd9668bf80b0a754780b79
Damyant Jain <damyant9999@gmail.com>
2018-06-11 19:04:40 +0530

    Enable

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/webhook/',function(req, res){
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            sendtext(sender, "Text echo: " + text.substring(0,100))
+            sendText(sender, "Text echo: " + text.substring(0,100))
         }
     }res.sendStatus(200)
 })
