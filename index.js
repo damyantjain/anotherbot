bd19b377ef033cb5b7ae253db1180c41cb67b48c
Damyant Jain <damyant9999@gmail.com>
2018-06-13 17:41:03 +0530

    Adding

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ function decideMessage(sender, text1) {
     {
         sendVideoMessage(sender)
     }
-   /* else if(text.includes("rainy"))
+    else if(text.includes("rainy"))
     {
-        sendGenericMessage(sender)
-    }*/
+        sendListMessage(sender)
+    }
     else if(text.includes("happy"))
     {
         sendText(sender, "Are you really happy")
@@ -181,6 +181,74 @@ function sendGenericMessage(sender){
     sendRequest(sender, messageData)
 }
 
+function sendListMessage(sender){
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "list",
+              "top_element_style": "compact",
+              "elements": [
+                {
+                  "title": "Classic T-Shirt Collection",
+                  "subtitle": "See all our colors",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                  "buttons": [
+                    {
+                      "title": "View",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                    }
+                  ]
+                },
+                {
+                  "title": "Classic White T-Shirt",
+                  "subtitle": "See all our colors",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall"
+                  }
+                },
+                {
+                  "title": "Classic Blue T-Shirt",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                  "subtitle": "100% Cotton, 200% Comfortable",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                  },
+                  "buttons": [
+                    {
+                      "title": "Shop Now",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                    }
+                  ]        
+                }
+              ],
+               "buttons": [
+                {
+                  "title": "View More",
+                  "type": "postback",
+                  "payload": "payload"            
+                }
+              ]  
+            }
+          }
+    }
+    sendRequest(sender, messageData)
+}
 
 function sendVideoMessage(sender){
     let messageData = {
