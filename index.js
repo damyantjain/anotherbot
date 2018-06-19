ba4e681503c5ea8985936607b0588c3f8dd709ad
Damyant Jain <damyant9999@gmail.com>
2018-06-19 15:53:08 +0530

    Adding

--- index.js
+++ index.js
@@ -263,67 +263,57 @@ function sendListMessage(sender){
         "attachment": {
             "type": "template",
             "payload": {
-              "template_type": "list",
-              "top_element_style": "compact",
-              "elements": [
+                "template_type": "list",
+                "top_element_style": "compact",
+                "elements": [
                 {
-                  "title": "Classic T-Shirt Collection",
-                  "subtitle": "See all our colors",
-                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                  "buttons": [
-                    {
-                      "title": "View",
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                    }
-                  ]
+                  "title": "Scarf",
+                    "subtitle": "$100",
+                    "image_url": "https://cdn.shopify.com/s/files/1/0923/0916/products/scarf_grande.png?v=1470769109",
+                    "default_action": {
+                        "type":"web_url",
+                        "url": "https://www.atticandbutton.com/products/scarf",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://www.atticandbutton.com/products/scarf"
+                    },
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Buy",
+                        "payload": JSON.stringify({
+                            "title":"Scarf",
+                            "url": "https://www.atticandbutton.com/products/scarf",
+                            "subtitle": "$100",
+                            "image_url": "https://cdn.shopify.com/s/files/1/0923/0916/products/scarf_grande.png?v=1470769109"
+                        })
+                    }]
                 },
                 {
-                  "title": "Classic White T-Shirt",
-                  "subtitle": "See all our colors",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                    "messenger_extensions": false,
-                    "webview_height_ratio": "tall"
-                  }
-                },
-                {
-                  "title": "Classic Blue T-Shirt",
-                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                  "subtitle": "100% Cotton, 200% Comfortable",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                    "messenger_extensions": true,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                  },
-                  "buttons": [
-                    {
-                      "title": "Shop Now",
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                    }
-                  ]        
-                }
-              ],
-               "buttons": [
-                {
-                  "title": "View More",
-                  "type": "postback",
-                  "payload": "rainy"            
-                }
-              ]  
-            }
-          }
-        }    
+                  "title": "Acrylic Cashmere Scarf",
+                    "subtitle": "$125",
+                    "image_url": "https://cdn.shopify.com/s/files/1/0923/0916/products/4195500_medium_3126d8ed-8d2c-4989-9e91-e2b18dafe6d7_grande.png?v=1470770727",
+                    "default_action": {
+                        "type":"web_url",
+                        "url": "https://www.atticandbutton.com/products/acrylic-cashmere-scarf",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://www.atticandbutton.com/products/acrylic-cashmere-scarf"
+                    },
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Buy",
+                        "payload": JSON.stringify({
+                            "title":"Acrylic Cashmere Scarf",
+                            "url": "https://www.atticandbutton.com/products/acrylic-cashmere-scarf",
+                            "subtitle": "$125",
+                            "image_url": "https://cdn.shopify.com/s/files/1/0923/0916/products/4195500_medium_3126d8ed-8d2c-4989-9e91-e2b18dafe6d7_grande.png?v=1470770727"
+                        })
+                    }]
+                }, 
+            ]
+        }
+    }
+           }    
     sendRequest(sender, messageData)
 }
 
