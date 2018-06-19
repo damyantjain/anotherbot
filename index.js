c65049b0a5b174a852601a921432bf65bcd61136
Damyant Jain <damyant9999@gmail.com>
2018-06-19 17:58:21 +0530

    Adding

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ function sendReceiptMessage(sender){
               "currency":"USD",
               "payment_method":"Visa 2345",        
               "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-              "timestamp":"1428444852",         
+              "timestamp":"1428444852",
               "address":{
                 "street_1":"1 Hacker Way",
                 "street_2":"",
