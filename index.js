0d979633fdaf90736205ee4d87a99000913fbcaf
Damyant Jain <damyant9999@gmail.com>
2018-06-19 13:54:45 +0530

    Adding

--- index.js
+++ index.js
@@ -159,8 +159,7 @@ function sendGenericMessage(sender){
         "attachment":{
             "type":"template",
             "payload":{
-              "template_type":"list",
-              "top_element_style": "compact",
+              "template_type":"generic",
               "elements":[
                  {
                   "title":"Welcome!",
@@ -193,59 +192,61 @@ function sendListMessage(sender){
                 {
                   "title": "Classic T-Shirt Collection",
                   "subtitle": "See all our colors",
-                  "image_url": "http://images6.fanpop.com/image/photos/36200000/snow-image-snow-36241624-500-375.png",          
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
                   "buttons": [
                     {
                       "title": "View",
                       "type": "web_url",
-                      "url": "https://google.com"           
+                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
                     }
                   ]
                 },
-             /*
                 {
                   "title": "Classic White T-Shirt",
                   "subtitle": "See all our colors",
                   "default_action": {
                     "type": "web_url",
-                    "url": "https://google.com",
-                    "messenger_extensions": true,
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    "messenger_extensions": false,
                     "webview_height_ratio": "tall"
                   }
                 },
-               {
+                {
                   "title": "Classic Blue T-Shirt",
-                  "image_url": "http://images6.fanpop.com/image/photos/36200000/snow-image-snow-36241624-500-375.png",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
                   "subtitle": "100% Cotton, 200% Comfortable",
                   "default_action": {
                     "type": "web_url",
-                    "url": "https://google.com",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
                     "messenger_extensions": true,
                     "webview_height_ratio": "tall",
-              
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
                   },
                   "buttons": [
                     {
                       "title": "Shop Now",
                       "type": "web_url",
-                      "url": "https://google.com",
+                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
                       "messenger_extensions": true,
                       "webview_height_ratio": "tall",
-                                  
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
                     }
                   ]        
-              }*/
+                }
               ],
-             /*  "buttons": [
+               "buttons": [
                 {
                   "title": "View More",
                   "type": "postback",
-                  "payload": "hey"            
+                  "payload": "rainy"            
                 }
-               ]  */
+              ]  
             }
           }
-    }
+        }    
     sendRequest(sender, messageData)
 }
 
